e185035906a8fca8c6706478e4ce0bc5cd73b45f
richie <accessrichard@gmail.com>
2022-03-05 09:50:23 -0600

    Removing config in .env.production

--- party_game/build_prod.sh
+++ party_game/build_prod.sh
@@ -1,4 +1,7 @@
 
+export REACT_APP_API_URL=//buzzgames.org
+export REACT_APP_SOCKET_URL=//buzzgames.org/socket
+export REACT_APP_API_TIMEOUT=5000
 export SECRET_KEY_BASE=`mix phx.gen.secret`
 export MIX_ENV=prod
 export PHX_HOST=buzzgames.org
