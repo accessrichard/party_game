e20aa03ea331007747f490465d46a5b999d1cb03
accessrichard <accessrichard@gmail.com>
2024-03-27 21:52:18 -0500

    more animation work

--- party-game-ui/src/features/hangman/hangmanCanvas.js
+++ party-game-ui/src/features/hangman/hangmanCanvas.js
@@ -1,12 +1,12 @@
 function to_radians(degrees)
 {
-  return degrees * (Math.PI/180);
+    return degrees * (Math.PI/180);
 }
 
 function rightArm(context, x, y, radius, angle, opts) {
     context.beginPath();
     context.moveTo(x, y);
-        context.lineTo(
+    context.lineTo(
         x + Math.cos(to_radians(angle)) * radius,
         y + Math.sin(to_radians(angle)) * radius
     );
@@ -83,6 +83,7 @@ function nose(context, x, y, radius, angle, opts) {
     context.restore();
 }
 
+
 function mouth(context, x, y, radius, angle, opts) {
     context.save();
     context.strokeStyle = "blue";
@@ -93,6 +94,12 @@ function mouth(context, x, y, radius, angle, opts) {
     context.stroke();
     context.fill();
     context.restore();
+}
+
+function smile(context, x, y, radius, angle, opts) {
+    context.beginPath();
+    context.arc(x, y - radius * .7, radius * .2, Math.PI, 0, true);
+    context.stroke();
 }
 
 function leftLeg(context, x, y, radius, angle, opts) {
@@ -162,7 +169,7 @@ function draw(x, y, radius, opts, cb) {
     context.clearRect(0, 0, canvas.width, canvas.height)
     
     if (opts && opts.drawCircles || true) {
-	drawCirclePaths(context, x, y, radius, opts);
+        drawCirclePaths(context, x, y, radius, opts);
     }
 
     body(context, x, y, radius, 0, opts);
@@ -182,40 +189,79 @@ function happyAnimation(context, x, y, radius, opts, then) {
     let accRightArm = 0;
     let accLeftLeg = 0;
     let accRightLeg = 0;
-    let step = 2;
-    let angle = 50;
-
+    let step = 4;
+    let angle = 150;
+    let optsCopy = {...opts}
+    
     draw(x, y, radius, opts)    
     
     function animate() {
         if (accLeftArm <= angle  * 5) {
-            [accLeftArm, opts.leftArmAngle] = bounceForward(accLeftArm, step, angle, 180);
+            [accLeftArm, opts.leftArmAngle] = bounceForward(accLeftArm, step, angle, optsCopy.leftArmAngle);
         }
 
         if (accRightArm <= angle * 4) {
-            [accRightArm, opts.rightArmAngle] = bounceReverse(accRightArm, step, angle, 330);
+            [accRightArm, opts.rightArmAngle] = bounceReverse(accRightArm, step, angle, optsCopy.rightArmAngle);
         }
 
         if (accLeftLeg <= angle * 4) {
-            [accLeftLeg, opts.leftLegAngle] = bounceReverse(accLeftLeg, step, angle, 60);
+            [accLeftLeg, opts.leftLegAngle] = bounceReverse(accLeftLeg, step, angle, optsCopy.leftLegAngle);
         }
 
         if (accRightLeg <= angle * 4) {
-            [accRightLeg, opts.rightLegAngle] = bounceReverse(accRightLeg, step, angle, 120);
+            [accRightLeg, opts.rightLegAngle] = bounceReverse(accRightLeg, step, angle, optsCopy.rightLegAngle);
         }
 
-	draw(x, y, radius, opts);
+        draw(x, y, radius, opts);
 
         if (accRightLeg <= angle * 4) {
-	    window.requestAnimationFrame(animate);
-	} else {
-	    then && then();
-	}
+        window.requestAnimationFrame(animate);
+        } else {
+        then && then();
+        }
     }
 
     animate();
 }
 
+function transitionBodyParts(context, x, y, radius, opts,  then) {
+    let leftArm = 180;
+    let rightArm = 330;
+    let leftLeg = 60;
+    let rightLeg = 120;
+
+    transition();
+
+    function transition() {
+
+        let animate = false;
+        if (opts.leftArmAngle <= leftArm) {
+        opts.leftArmAngle += 5;
+        animate = true;
+        }
+
+        if (opts.rightArmAngle >= 0) {
+        opts.rightArmAngle -= 5;
+        animate = true;
+        }
+
+        if (opts.leftLegAngle >= leftLeg) {
+        opts.leftLegAngle -= 5;
+        animate = true;
+        }
+
+        if (opts.rightLegAngle <= rightLeg) {
+            opts.rightLegAngle += 5;
+            animate = true;
+        }
+        draw(x, y, radius, opts);
+        if (animate) {
+        window.requestAnimationFrame(transition);
+        } else {
+        then && then();
+        }
+    }
+}
 
 function walk(context, x, y, radius, opts, to, then) {
     let accLeftArm = 0;
@@ -234,18 +280,18 @@ function walk(context, x, y, radius, opts, to, then) {
     function walkAnimate() {
         [accLeftArm, opts.leftArmAngle] = bounceForward(accLeftArm, step, 60, 45);
         [accRightArm, opts.rightArmAngle] = bounceReverse(accRightArm, step, 60, 120);
-	[accLeftLeg, opts.leftLegAngle] = bounceForward(accLeftLeg, step, 60, 60);
+        [accLeftLeg, opts.leftLegAngle] = bounceForward(accLeftLeg, step, 60, 60);
         [accRightLeg, opts.rightLegAngle] = bounceReverse(accRightLeg, step, 60, 120);
 
-	x +=1;
+        x +=step;
 	
-	draw(x, y, radius, opts);
+        draw(x, y, radius, opts);
 
-	if (x < to) {
-	    window.requestAnimationFrame(walkAnimate);
-	} else {
-	    then && then();
-	}
+        if (x < to) {
+        window.requestAnimationFrame(walkAnimate);
+        } else {
+        then && then();
+        }
     }
 
     walkAnimate();
@@ -253,25 +299,25 @@ function walk(context, x, y, radius, opts, to, then) {
 
 
 window.onload = () => {
-    let radius = 30;
+    let radius = 150;
     let opts = {
 	torso: radius * .8,
 	leftArmAngle: 180,
 	rightArmAngle: 330,
-	leftLegAngle: 60,
-	rightLegAngle: 120
+	leftLegAngle: 120,
+	rightLegAngle: 60
     };
 
     const canvas = document.getElementById("hangman");
     const context = canvas.getContext("2d");
     
-    const happy = () => { happyAnimation(context, 600, 200, radius, opts, walk2);}
-    const walk2 = () => { walk(context, 600, 200, radius, opts, 800);}
-    
-    walk(context, 200, 200, radius, opts, 600, happy);
-
-
-
+    const happy = () => { happyAnimation(context, x + 50, y, radius, opts, walk2);}
+    const walk2 = () => { walk(context, x + 50, y, radius, opts, canvas.width + radius * 2);}
+    const transition = () => { transitionBodyParts(context, x + 50, y, radius, opts, happy)};
+    let x = 200;
+    let y = 200;
+    draw(x, y, radius, opts)
+    walk(context, x, y, radius, opts, 250, transition);
 };
 
 
