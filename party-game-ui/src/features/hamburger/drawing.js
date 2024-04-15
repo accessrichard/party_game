eab4587b56ff0c29869b8df0ea8e51f41a6cb551
accessrichard@gmail.com <accessrichard@gmail.com>
2024-04-15 15:42:53 -0500

    Adding movement

--- party-game-ui/src/features/hamburger/drawing.js
+++ party-game-ui/src/features/hamburger/drawing.js
@@ -1,5 +1,3 @@
-
-
 class Sprite {
     shift = 0;
     frameWidth = 80;
@@ -21,10 +19,17 @@ class Sprite {
 
     }
 
+    mirror(canvas, context, x = 0, y = 0, horizontal = true, vertical = false) {
+        context.save();
+        context.scale(-1, 1);
+        this.draw(context, x * -1, y);
+        context.restore();
+    }
+
     draw(context, x, y) {
         context.drawImage(
             this.image,
-            this.shift, 
+            this.shift,
             0,
             this.frameWidth,
             this.frameHeight,
@@ -37,43 +42,89 @@ class Sprite {
         if (this.currentFrame == this.totalFrames) {
             this.shift = 0;
             this.currentFrame = 0;
-          }
-         
-          this.currentFrame++;
-    }
+        }
 
+        this.currentFrame++;
+    }
 }
 
-window.onload = draw;
-
-
-
-
-
-
-
-let walk = new Sprite("./hamburger_walk.png", 80, 100, 7);
-let stand = new Sprite("./hamburger_idle.png", 80, 100, 1);
-let i =0;
-function draw() {
-    const canvas = document.getElementById("hamburger");
-    const context = canvas.getContext("2d");
-    
-    context.clearRect(0, 0, 800, 400)
-   
-
-    if (i > 800) {
-        i=0;
-    }
-
-    if (i < 400) {
-        walk.draw(context, i, 100);
-        i+=5;    
-    } else {
-        stand.draw(context, 400, 100);
-    }
-
-    requestAnimationFrame(draw)
+window.onload = () => {
+    var game = new Hamburger();
+    game.addEvents();
+    game.draw();
 }
 
+class Hamburger {
 
+    playerX = 100;
+    playerY = 100;
+    isWalking = false;
+    isForward = true;
+    isJumping = false;
+
+    constructor() {
+        this.canvas = document.getElementById("hamburger");
+        this.context = this.canvas.getContext("2d");
+        this.walk = new Sprite("./hamburger_walk.png", 80, 100, 7);
+        this.stand = new Sprite("./hamburger_idle.png", 80, 100, 1);
+    }
+
+    addEvents() {
+        this.canvas.addEventListener('pointerdown', this.mouseDown.bind(this));
+        this.canvas.addEventListener('pointermove', this.mouseMove.bind(this));
+        this.canvas.addEventListener('pointerup', this.mouseUp.bind(this));
+        document.addEventListener('keydown', this.keyDown.bind(this));
+    }
+
+    keyDown(e) {
+        if (e.keyCode === 32) {
+            this.isJumping = true;
+        }
+    }
+
+    mouseDown(e) {
+        this.isWalking = true;
+        this.isForward = e.layerX >= this.playerX;
+    }
+
+    mouseMove(e) {
+        if (this.isWalking) {
+            this.isForward = e.layerX >= this.playerX;
+        }
+    }
+
+    mouseUp() {
+        this.isWalking = false;
+    }
+
+
+    draw() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+        if (this.isJumping) {
+            this.playerY -= 3;
+            if (this.playerY < 50) {
+                this.isJumping = false;                
+            }
+        }
+
+        if (!this.isJumping && this.playerY < 100) {
+            this.playerY +=3;
+        }
+
+        if (this.isWalking) {
+            this.playerX += (this.isForward ? 1 : -1) * 3;
+            if (this.isForward) {
+                this.walk.draw(this.context, this.playerX, this.playerY);
+            } else {
+                this.walk.mirror(this.canvas, this.context, this.playerX, this.playerY);
+            }
+
+        } else {
+            this.stand.draw(this.context, this.playerX, this.playerY);
+        }
+
+
+        requestAnimationFrame(this.draw.bind(this))
+    }
+}
