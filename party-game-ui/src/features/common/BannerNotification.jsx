b24fee9e23af6276b50e3703e7daebde0de6b81c
accessrichard <accessrichard@gmail.com>
2025-10-29 18:11:05 -0500

    More socket disconnect fixes

--- party-game-ui/src/features/common/BannerNotification.jsx
+++ party-game-ui/src/features/common/BannerNotification.jsx
@@ -1,46 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { SOCKET_DISCONNECTED, SOCKET_ERROR, socketDisconnect  } from '../phoenix/phoenixMiddleware';
+import { useSelector } from 'react-redux';
+import { SOCKET_DISCONNECTED, SOCKET_ERROR } from '../phoenix/phoenixMiddleware';
 import Timer from './Timer';
-import { push } from "redux-first-history";
-
-
 
 export default function BannerNotification(props) {
-    const { text = "Network Error. Trying to Reconnect" } = props;
 
-    const dispatch = useDispatch();
+    const { text = "Network Error. Trying to Reconnect" } = props;
     const socketStatus = useSelector(state => state.phoenix.socket.status);
-    const [isVisible, setIsVisible] = useState(false);
     const [isTimerActive, setIsTimerActive] = useState(false);
 
     useEffect(() => {
         if (socketStatus === SOCKET_DISCONNECTED || socketStatus === SOCKET_ERROR) {
-            setIsVisible(true)
             setIsTimerActive(true)
         } else {
-            setIsVisible(false)
             setIsTimerActive(false)
         }
-
     }, [socketStatus]);
 
 
     function onTimerCompleted() {
-        dispatch(socketDisconnect());
-        dispatch(push("/"));
+        window.location.href = "/";
     }
 
     return (
         <>
-            {isVisible && <div className="header-notification larger-title flex-grid flex-center ">
+            {isTimerActive && <div className="header-notification larger-title flex-grid flex-center ">
 
                 <span className='pd-5'>{text}</span>
 
                 <Timer className='pd-5'
                     numberSeconds={30}
                     isIncrement={false}
-                    isVisible={isVisible}
+                    isVisible={isTimerActive}
                     isActive={isTimerActive}
                     timeFormat="seconds"
                     onTimerCompleted={onTimerCompleted}
